0a0ceb1b165a0fcc6a807911b34c010642241736
seokjeongeum <jeongeumseok@naver.com>
2021-08-28 15:37:25 +0900

    Send texts

--- src/main/java/org/example/kakaoworkbot/Main.java
+++ src/main/java/org/example/kakaoworkbot/Main.java
@@ -1,6 +1,7 @@
 package org.example.kakaoworkbot;
 
 import com.google.gson.*;
+import org.example.kakaoworkbot.models.Index;
 import org.example.kakaoworkbot.models.User;
 
 import java.net.URI;
@@ -12,13 +13,16 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+//TODO: Make wrapper classes instead of using JsonObject::get
 public class Main {
     private static String appKey;
     private static User[] users;
+    private static String fmpApiKey;
 
     public static void main(String[] args) {
         appKey = System.getenv("APP_KEY");
         users = GetUsers();
+        fmpApiKey = System.getenv("FMP_API_KEY");
         SendClosingPrices();
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -43,12 +47,17 @@ public class Main {
         }, usaStockMarketTodayClosingTime.getTime(), 24 * 60 * 60 * 1000);
     }
 
+    //TODO: Separate getting conversation ids
     private static void SendClosingPrices() {
         URI conversationsOpen;
         URI messagesSend;
+        URI nasdaq100Api;
+        URI snp500Api;
         try {
             conversationsOpen = new URI("https://api.kakaowork.com/v1/conversations.open");
             messagesSend = new URI("https://api.kakaowork.com/v1/messages.send");
+            nasdaq100Api = new URI("https://financialmodelingprep.com/api/v3/quote/%5ENDX?apikey=" + fmpApiKey);
+            snp500Api = new URI("https://financialmodelingprep.com/api/v3/quote/%5EGSPC?apikey=" + fmpApiKey);
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return;
@@ -58,6 +67,13 @@ public class Main {
         Gson gson = new GsonBuilder()
                 .setPrettyPrinting()
                 .create();
+        var nasdaq100Body = httpClient.sendAsync(HttpRequest.newBuilder(nasdaq100Api).build(), HttpResponse.BodyHandlers.ofString())
+                .join()
+                .body();
+        var snp500Body = httpClient.sendAsync(HttpRequest.newBuilder(snp500Api).build(), HttpResponse.BodyHandlers.ofString())
+                .join()
+                .body();
+        var indexes = new Index[]{gson.fromJson(nasdaq100Body, Index[].class)[0], gson.fromJson(snp500Body, Index[].class)[0]};
         List<CompletableFuture<String>> responses = Arrays.stream(users).parallel()
                 .map(user -> {
                     Map<Object, Object> data = new HashMap<>();
@@ -82,20 +98,30 @@ public class Main {
                 }).map(jsonObject -> jsonObject.get("conversation").getAsJsonObject()
                         .get("id").getAsString())
                 .map(conversationId -> {
-                    Map<Object, Object> data = new HashMap<>();
-                    data.put("conversation_id", conversationId);
-                    data.put("text", "text");
-                    HttpRequest httpRequest = HttpRequest.newBuilder(messagesSend)
-                            .header("Authorization", "Bearer " + appKey)
-                            .header("Content-Type", "application/json")
-                            .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(data)))
-                            .build();
-                    return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
-                            .thenApply(HttpResponse::body);
+                    ArrayList<CompletableFuture<String>> futures = new ArrayList<>();
+                    for (Index index : indexes) {
+                        Map<Object, Object> data = new HashMap<>();
+                        data.put("conversation_id", conversationId);
+                        //TODO: Change the text color subject to the price change
+                        data.put("text", String.format("%s: %.3f (%.3f, %.2f%%)", index.name, index.price, Math.abs(index.change), Math.abs(index.changesPercentage)));
+                        HttpRequest httpRequest = HttpRequest.newBuilder(messagesSend)
+                                .header("Authorization", "Bearer " + appKey)
+                                .header("Content-Type", "application/json")
+                                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(data)))
+                                .build();
+                        futures.add(httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                                .thenApply(HttpResponse::body));
+                    }
+                    return futures;
                 })
+                .flatMap(Collection::parallelStream)
                 .collect(Collectors.toList());
         for (CompletableFuture<String> response : responses) {
-            System.out.println(gson.toJson(response.join()));
+            JsonObject jsonObject = JsonParser.parseString(response.join()).getAsJsonObject();
+            boolean success = jsonObject.get("success").getAsBoolean();
+            if (!success) {
+                System.out.println(gson.toJson(jsonObject.get("error")));
+            }
         }
     }
 
---
+++ src/main/java/org/example/kakaoworkbot/models/Index.java
@@ -0,0 +1,8 @@
+package org.example.kakaoworkbot.models;
+
+public class Index {
+    public String name;
+    public double price;
+    public double changesPercentage;
+    public double change;
+}
